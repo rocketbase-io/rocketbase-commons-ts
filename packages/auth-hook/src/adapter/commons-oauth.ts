095b3e09dda62e8c8a89f15a90d8d5b5942f16d2
Marten <marten@rocketbase.io>
2021-10-11 17:38:04 +0200

    fix: prepared for release

--- packages/auth-hook/src/adapter/commons-oauth.ts
+++ packages/auth-hook/src/adapter/commons-oauth.ts
@@ -1,35 +1,26 @@
-import { AuthOptions, AuthProviderClass, ProviderOptions } from 'react-use-auth/dist/types';
-import { AppUserToken, TokenResponse } from '../../../auth-client/src';
-import random from '../lib/random';
-import qs from 'qs';
-import axios from 'axios';
+import { AuthOptions, AuthProviderClass, AuthResult, ProviderOptions } from 'react-use-auth/dist/types';
+import { AppUserToken, OauthClient, OAuthProps } from '@rocketbase/commons-auth-api';
 
-export interface CommonsOauthProps {
-  clientId?: string;
+export interface CommonsOauthProps extends OAuthProps {
   server: string;
-  redirectUri?: string;
-  authorizationUri?: string;
-  tokenUri?: string;
-  userinfoUri?: string;
-  endSessionUri?: string;
-  signupUri?: string;
-  logoutUri?: string;
-  scopes?: string;
   /**
    * used after login when previous url couldn't got
    */
   fallbackRedirectUri?: string;
+  signupUri?: string;
 }
 
 export class CommonsOauth implements AuthProviderClass {
   private readonly dispatch: (eventName: string, eventData?: any) => void;
   private readonly config: CommonsOauthProps;
+  private readonly client: OauthClient;
 
   // Initialize the client and save any custom config
   constructor(params: AuthOptions) {
     // You will almost always need access to dispatch
     this.dispatch = params.dispatch;
     this.config = params;
+    this.client = new OauthClient(params);
   }
 
   // Makes configuration easier by guessing default options
@@ -49,68 +40,39 @@ export class CommonsOauth implements AuthProviderClass {
   }
 
   public authorize() {
-    const id = random();
-    localStorage.setItem(id, window.location.href);
-
-    const params = qs.stringify({
-      client_id: this.config.clientId,
-      redirect_uri: this.config.redirectUri,
-      response_type: 'code',
-      scope: this.config.scopes,
-      state: id
-    });
-
-    window.location.href = `${this.config.authorizationUri}?${params}`;
+    this.client.authorize();
   }
 
   public signup() {
-    window.open(this.config.signupUri!, '_blank');
+    if (this.config.signupUri) {
+      window.open(this.config.signupUri, '_blank');
+    }
   }
 
   public logout(returnTo?: string) {
-    localStorage.removeItem('refreshToken');
-    window.location.href  = this.config.logoutUri!;
-    // Logout should be done in a separate windows that auto-close afterwards...
+    this.client.logout();
   }
 
   public async handleLoginCallback(): Promise<boolean> {
     return new Promise((resolve, reject) => {
-      const params = qs.parse(window.location.search);
-      if ('state' in params && 'code' in params) {
-        const requestParams = qs.stringify({ grant_type: 'authorization_code', code: params.code });
-        try {
-          axios.get<TokenResponse>(`${this.config.tokenUri}?${requestParams}`)
-            .then(({ data: tokenResponse }) => {
-              const authResult = {
-                expiresIn: tokenResponse.expires_in,
-                accessToken: tokenResponse.access_token
-              };
-              localStorage.setItem('accessToken', authResult.accessToken);
-              localStorage.setItem('refreshToken', tokenResponse.refresh_token);
+      this.client.parseAuthCallback()
+        .then(({ user, ...callback }) => {
+          const authResult = {
+            expiresIn: callback.tokenExpiresIn,
+            ...callback
+          };
 
-              axios.get<AppUserToken>(`${this.config.userinfoUri}`, { headers: { 'Authorization': `Bearer ${tokenResponse.access_token}` } })
-                .then(({ data: userInfo }) => {
-                  this.dispatch('AUTHENTICATED', { authResult, userInfo });
-                  const state = params.state as string;
-                  const redirectUri = localStorage.getItem(state);
-                  localStorage.removeItem(state);
-                  window.location.href = redirectUri || this.config.fallbackRedirectUri || "/";
-                });
-            });
-        } catch (err) {
-          this.dispatch('ERROR', {
+          this.dispatch('AUTHENTICATED', { authResult, user });
+          resolve(true);
+        })
+        .catch(err => {
+
+          this.dispatch("ERROR", {
             error: err,
-            errorType: 'authResult'
+            errorType: "handleAuth"
           });
           resolve(false);
-        }
-      } else {
-        this.dispatch('ERROR', {
-          error: 'state missing',
-          errorType: 'authResult'
         });
-        resolve(false);
-      }
     });
   }
 
@@ -122,34 +84,19 @@ export class CommonsOauth implements AuthProviderClass {
     return user?.capabilities || [];
   }
 
-  public async checkSession(): Promise<{
-    user: AppUserToken;
-    authResult: any;
-  }> {
-       return new Promise((resolve, reject) => {
-         const refreshToken = localStorage.getItem('refreshToken');
-         if (refreshToken) {
-           const requestParams = qs.stringify({ grant_type: 'refresh_token', refresh_token: refreshToken });
-           axios.get<TokenResponse>(`${this.config.tokenUri}?${requestParams}`)
-             .then(({ data: tokenResponse }) => {
-               const authResult = {
-                 expiresIn: tokenResponse.expires_in,
-                 accessToken: tokenResponse.access_token
-               };
-               localStorage.setItem('accessToken', authResult.accessToken);
-
-               axios.get<AppUserToken>(`${this.config.userinfoUri}`, { headers: { 'Authorization': `Bearer ${tokenResponse.access_token}` } })
-                 .then(({ data: userInfo }) => {
-                   resolve({user: userInfo, authResult});
-                 });
-             })
-             .catch((err) => {
-               localStorage.removeItem('refreshToken');
-               reject(err);
-             });
-         } else {
-           reject();
-         }
-       });
-    }
+  public async checkSession(): Promise<{ user: AppUserToken; authResult: AuthResult; }> {
+    return new Promise((resolve, reject) => {
+      this.client.checkSession()
+        .then(({user, ...callback}) => {
+          const authResult = {
+            expiresIn: callback.tokenExpiresIn,
+            ...callback
+          };
+          resolve({user, authResult});
+      })
+        .catch(err => {
+          reject(err || new Error("Session invalid"));
+        })
+    });
+  }
 }
